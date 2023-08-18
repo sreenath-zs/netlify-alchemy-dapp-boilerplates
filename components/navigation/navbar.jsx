0d6c69749741c6363c99a6922427e350a6cb4bae
sreenath-zs <sreenath@zelarsoft.com>
2023-08-18 22:39:34 +0530

    logi

--- components/navigation/navbar.jsx
+++ components/navigation/navbar.jsx
@@ -4,7 +4,13 @@ export default function Navbar() {
 	return (
 		<nav className={styles.navbar}>
 			<a href="http://sreenathrk.online/" target={"_blank"}>
-				<img className={styles.alchemy_logo} align="center" src="/sr.png"></img>
+				{/* <img className={styles.alchemy_logo} align="center" src="/sr.png"></img> */}
+				<img
+						id="badge-button"
+						style={{ width: "240px", height: "53px" }}
+						src="/sr.png"
+						alt="Alchemy Supercharged"
+					/>
 			</a>
 			{/* <ConnectButton></ConnectButton> */}
 		</nav>
