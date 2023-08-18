0421eabcfe1d0237dfcfcfd72129286351a0ff97
sreenath-zs <sreenath@zelarsoft.com>
2023-08-18 19:42:34 +0530

    added name

--- components/InstructionsComponent.jsx
+++ components/InstructionsComponent.jsx
@@ -6,10 +6,10 @@ export default function InstructionsComponent() {
 		<div className={styles.container}>
 			<header className={styles.header_container}>
 				<h1>
-					create<span>-web3-dapp</span>
+					Sreenath Rangana<span>-DevOps-Engineer</span>
 				</h1>
 				<p>
-					Get started by editing this page in{" "}
+					Website is working under working.....{" "}
 					<span>/pages/index.js</span>
 				</p>
 			</header>
